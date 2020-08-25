f7a4c0d5479405c4bef72a7ab0f3976874e9c864
ksllive <zhenya-kiselev-97@mail.ru>
2020-08-25 12:59:27 +0300

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Eugene Kiselev on 25.08.2020.
+//  Copyright © 2020 Eugene Kiselev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

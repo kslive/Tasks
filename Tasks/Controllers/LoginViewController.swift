2821767d950439d4b0a1e4dd98d755443e99aa0d
ksllive <zhenya-kiselev-97@mail.ru>
2020-08-25 16:15:39 +0300

    bug fix.

--- Tasks/Controllers/LoginViewController.swift
+++ Tasks/Controllers/LoginViewController.swift
@@ -13,10 +13,12 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var warningLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setObserver()
     }
 
     @IBAction func loginButton(_ sender: UIButton) {
@@ -25,5 +27,27 @@ class LoginViewController: UIViewController {
     @IBAction func registerButton(_ sender: UIButton) {
     }
     
+    // MARK: - Help Function
+    
+    func setObserver() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow(notification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    @objc func kbDidShow(notification: Notification) {
+        
+        guard let userInfo = notification.userInfo else { return }
+        
+        let kbFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        scrollView.contentSize = CGSize(width: viewInSV.bounds.size.width, height: viewInSV.bounds.size.height + kbFrameSize.height)
+        scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbFrameSize.height, right: 0)
+    }
+    
+    @objc func kbDidHide() {
+        
+        scrollView.contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+    }
 }
 

52b2e2f16284eacb12d3b6822d58e7f5e62a163f
ksllive <zhenya-kiselev-97@mail.ru>
2020-08-25 15:01:47 +0300

    Keyboard setup.

--- Tasks/Controllers/LoginViewController.swift
+++ Tasks/Controllers/LoginViewController.swift
@@ -16,12 +16,37 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     @IBAction func loginButton(_ sender: UIButton) {
     }
     
     @IBAction func registerButton(_ sender: UIButton) {
+    }
+    
+    // MARK: Help Function
+    
+    func setObserver() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        
+    }
+    
+    @objc func kbDidShow(notification: Notification) {
+        
+        guard let userInfo = notification.userInfo else { return }
+        
+        let kbFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + kbFrameSize.height)
+        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbFrameSize.height, right: 0)
+    }
+    
+    @objc func kbDidHide() {
+        
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
     }
 }
 
